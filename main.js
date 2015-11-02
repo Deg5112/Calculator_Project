b9934d509da1d5d81efe486b5c9f87bad4398541
David Goodman <deg5112@gmail.com>
2015-11-02 09:58:50 -0800

    need to finish logic for array

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
 var callback = function(value){
-    switch(value){
-        case 'undefined':
-            $('#display').val('');
-            break;
-        default:
-            $('#display').val(value);
+    //switch(value){
+    //    case 'undefined':
+    //        $('#display').val('');
+    //        break;
+    //    default:
+    //        $('#display').val(value);
+        console.log('new calc created');
     }
-};
+
 
 var myCalc = new calculator(callback);
 
 
 $(function(){
    $('button').click(function(){
+       console.log('clicked');
      var val = $(this).text();
+       console.log ('val clicked: ' + val);
        switch(val){
            case 'AC':
                myCalc.allClear();
--- object.js
+++ object.js
@@ -1,13 +1,89 @@
-var calculator = function(callback) {
+var calculator = function(callback){
     var self = this;
-    self.c = callback;
     self.arr = [];
+    self.allClear = function(){
+        $('#display').val('');
+        self.arr = [];
+        console.log(self.arr);
+    }
+    self.addToScreen = function(val){
+        console.log('add function running');
+        switch(val){
+            case '%':
 
 
-//allclear erases array and resets the display to none because we have no path for it , it's undefined
-    self.addToScreen = function (val) {
-        console.log('hello');
-        console.log('val');
-        self.c(val);
-    };
-};
+                break;
+            case '+/-':
+
+                break;
+
+            case '.':
+
+                break;
+
+            case '/':
+                if(self.arr.length > 0 && self.arr.length < 2){
+                    self.arr.push(val);
+                    console.log(self.arr);
+                    $('#display').val(val);
+                }
+
+                break;
+
+            case 'x':
+                if(self.arr.length > 0 && self.arr.length < 2){
+                    self.arr.push(val);
+                    console.log(self.arr);
+                    $('#display').val(val);
+                }
+
+                break;
+
+            case '-':
+                if(self.arr.length > 0 && self.arr.length < 2){
+                    self.arr.push(val);
+                    console.log(self.arr);
+                    $('#display').val(val);
+                }
+
+                break;
+
+            case '+':
+                if(self.arr.length > 0 && self.arr.length < 2){
+                    self.arr.push(val);
+                    console.log(self.arr);
+                    $('#display').val(val);
+                }
+
+                break;
+
+            case '=':
+
+                break;
+
+            default:
+                if( isNaN(parseFloat(val) ) ){ //if not a number, check..
+                    console.log('not a number');
+                    console.log('self arr length: ' + self.arr.length);
+                    if(self.arr.length>0 && self.arr.length < 2){
+
+                    }
+
+                }
+                if(self.arr.length<1) {// if no items in array
+                    $('#display').val(val);
+                    $(self.arr.push(val));
+                    console.log(self.arr);
+                    break;
+                }else{
+                    if(self.arr.length<2){ //concat if you click nums consecutively and only one item in array
+                        var concat = self.arr[0] + val;
+                        self.arr[0] = concat;
+                        $('#display').val(concat);
+                        console.log(self.arr);
+                    }
+                }
+            }
+         }
+    }
+
