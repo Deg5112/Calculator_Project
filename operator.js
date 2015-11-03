58362c4aa443e7160af545cf1a77e071e5b9a0d9
David Goodman <deg5112@gmail.com>
2015-11-03 01:21:33 -0800

    combo operators done!

--- operator.js
+++ operator.js
@@ -11,7 +11,7 @@ calculator.prototype.operator = function(value){
                     self.c(quotient, 'quotient');
                     self.arr = [];
                     var curCalculation = new calculationItem(quotient, 'quotient');
-                    self.arr.push(curCalculation, curOperator);// [ {sum}, {+}, {?} }
+                    self.arr.push(curCalculation);// [ {sum}, {+}, {?} }
                     self.num1 = curCalculation.calculation;
                 };
                 //do this if length = 1
@@ -26,7 +26,7 @@ calculator.prototype.operator = function(value){
                     self.arr = [];
                     var curCalculation = new calculationItem(product, 'product');
                     self.arr.push(curCalculation);// [ {sum}, {+}, {?} }
-                    console.log(self.arr);
+                    
                     self.num1 = curCalculation.calculation;
 
                 };
@@ -41,7 +41,7 @@ calculator.prototype.operator = function(value){
                     self.c(difference, 'difference');
                     self.arr = [];
                     var curCalculation = new calculationItem(difference, 'difference');
-                    self.arr.push(curCalculation, curOperator);// [ {sum}, {+}, {?} }
+                    self.arr.push(curCalculation);// [ {sum}, {+}, {?} }
                     self.num1 = curCalculation.calculation;
                 };
                 //do this if length = 1
@@ -56,10 +56,9 @@ calculator.prototype.operator = function(value){
                     self.c(sum, 'sum');//call function have it put sum on screen
                     self.arr = [];
                     var curCalculation = new calculationItem(sum, 'sum');//make object, clear array, push into array
-                    self.arr.push(curCalculation, curOperator);// [ {sum}, {+}, {?} }
+                    self.arr.push(curCalculation);// [ {sum}, {+}, {?} }
                     self.num1 = curCalculation.calculation;
-                    console.log(self.arr);
-
+                    
                 };
                 //do this if length = 1
                 self.arr.push(curOperator);//no calculation made, just push operator into middle slot of array
@@ -71,20 +70,73 @@ calculator.prototype.operator = function(value){
 
     //what is it.. self.calculate if not equal.. makes the calculation object, makes it arr[0], puts it on the screen; and then stick the new operation in the middle
     //if we press the equals and there's three items in array/ with a variable operation
+
+    //TODO we had to readd the method to the object when replacing that as the middle operator in the array. maybe make the plus objects and so on?
     if(length === 3) {
-        if(value !== '=') { //if      + - / *
-            self.calculate(self.num1, self.num2);
+       switch(value){
+           case '+':
+               self.calculate(self.num1, self.num2);
+               curOperator.operation = function(num1, num2){////if equals operator is hit on three this method is also ran
+                   var sum = num1 + num2;
+                   self.c(sum, 'sum');//call function have it put sum on screen
+                   self.arr = [];
+                   var curCalculation = new calculationItem(sum, 'sum');//make object, clear array, push into array
+                   self.arr.push(curCalculation);// [ {sum}, {+}, {?} }
+                   self.num1 = curCalculation.calculation;
+                   
+               };
+               self.arr.push(curOperator);
+               
+               break;
 
-            self.arr.push(curOperator);
+           case '-':
+               self.calculate(self.num1, self.num2);
+               curOperator.operation = function(num1, num2){
+                   var difference = num1 - num2;
+                   self.c(difference, 'difference');
+                   self.arr = [];
+                   var curCalculation = new calculationItem(difference, 'difference');
+                   self.arr.push(curCalculation);// [ {sum}, {+}, {?} }
+                   self.num1 = curCalculation.calculation;
+               };
+               self.arr.push(curOperator);
+               
+               break;
 
+           case '/':
+               self.calculate(self.num1, self.num2);
+               curOperator.operation = function(num1, num2){
+                   var quotient = num1/num2;
+                   self.c(quotient, 'quotient');
+                   self.arr = [];
+                   var curCalculation = new calculationItem(quotient, 'quotient');
+                   self.arr.push(curCalculation);// [ {sum}, {+}, {?} }
+                   self.num1 = curCalculation.calculation;
+               };
+               self.arr.push(curOperator);
+               
+               break;
 
+           case 'x':
+               self.calculate(self.num1, self.num2);
+               curOperator.operation = function(num1, num2){
+                   var product = num1 * num2;
+                   self.c(product, 'product');
+                   self.arr = [];
+                   var curCalculation = new calculationItem(product, 'product');
+                   self.arr.push(curCalculation);// [ {sum}, {+}, {?} }
+                   
+                   self.num1 = curCalculation.calculation;
+               };
+               self.arr.push(curOperator);
+               
+               break;
 
-        }else{
-
-            self.calculate(self.num1, self.num2);
-
-
-        }
+           default:
+                self.calculate(self.num1, self.num2);
+               
+               break;
+       }
     }
 };
 
