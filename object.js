8fedb584fbca726cf762b70f9a91661860a79fe6
David Goodman <deg5112@gmail.com>
2015-11-02 23:55:37 -0800

    update to decimal

--- object.js
+++ object.js
@@ -11,31 +11,41 @@ var calculator = function(callback) {
     self.num2;
 
     self.addItem = function(value){
-           if( isNaN(parseFloat(value)) ) {//if not a number
-               var objVal = self.arr[(self.arr.length)-1].value;
+
+           if( isNaN(parseFloat(value) ) ) {//if not a number,return true
                 switch(value){
                     case '+/-':
+                        if(self.arr.length>=1){
+                            var objVal = self.arr[(self.arr.length)-1].value;
                             objVal *= -1;
                             self.arr[(self.arr.length)-1].value = objVal;
                             self.c(objVal);
+                        }
+
                         break;
                     case '%':
-                        objVal /= 100;
-                        self.arr[(self.arr.length)-1].value = objVal;
-                        self.c(objVal);
-                        break;
+                        if(self.arr.length>=1){
+                            var objVal = self.arr[(self.arr.length)-1].value;
+                            objVal /= 100;
+                            self.arr[(self.arr.length)-1].value = objVal;
+                            self.c(objVal);
+                            break;
+                        }
                     case '.':
-
-                        if(objVal%1===0){
-                            var result = objVal + '.';
-                            self.arr[(self.arr.length)-1].value = result;
-                            self.c(result);
-                            self.num1 = result;
+                        if(self.arr.length>=1){
+                            if (objVal % 1 === 0) {
+                                var result = objVal + '.';
+                                console.log(result);
+                                self.arr[(self.arr.length)-1].value = result;
+                                self.c(result);
+                                self.num1 = result;
+                            }
                         }
                         break;
 
                     default:
                         self.operator(value);
+                            break;
                 }
            }else{//if number
                self.addNum(value);
